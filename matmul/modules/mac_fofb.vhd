7669c3121a1a5a1b9689e22f5d64271b51ab7e03
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2021-07-21 08:19:43 -0300

    mac_fofb: core review and separing input data

--- matmul/modules/mac_fofb.vhd
+++ matmul/modules/mac_fofb.vhd
@@ -1,34 +1,19 @@
-------------------------------------------------------------------------------
-
+-------------------------------------------------------------------------------
 -- Title      :  Matrix multiplication core
-
-------------------------------------------------------------------------------
-
+-------------------------------------------------------------------------------
 -- Author     :  Melissa Aguiar
-
 -- Company    : CNPEM LNLS-DIG
-
 -- Platform   : FPGA-generic
-
 -------------------------------------------------------------------------------
-
 -- Description:  Matrix multiplication core top level for the Fast Orbit Feedback
-
 -------------------------------------------------------------------------------
-
 -- Copyright (c) 2020 CNPEM
-
 -- Licensed under GNU Lesser General Public License (LGPL) v3.0
-
 -------------------------------------------------------------------------------
-
 -- Revisions  :
-
 -- Date        Version  Author                Description
-
--- 2021-19-07  1.0      melissa.aguiar        Created
-
-------------------------------------------------------------------------------
+-- 2021-20-07  1.0      melissa.aguiar        Created
+-------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.ALL;
@@ -38,7 +23,6 @@ library work;
 use work.mult_pkg.all;
 
 entity mac_fofb is
-
   generic(
     -- Width for input a[k]
     g_a_width                           : natural := 32;
@@ -50,8 +34,6 @@ entity mac_fofb is
     g_c_width                           : natural := 32;
     -- Extra bits for accumulator
     g_extra_width                       : natural := 4;
-    -- Size of incoming data packet
-    g_packet_size                       : natural := 73;
     -- Number of products
     g_mac_size                          : natural := 160
   );
@@ -63,15 +45,17 @@ entity mac_fofb is
     rst_n_i                             : in std_logic;
     -- Data valid input
     valid_i                             : in std_logic;
-    -- Packet input (id, xpos, ypos)
-    fod_dat_i                           : in std_logic_vector(g_packet_size-1 downto 0);
+    -- Input a[k]
+    coeff_a_dat_i                       : in signed(g_a_width-1 downto 0);
     -- Input b[k]
-    coeff_x_dat_i                       : in signed(g_b_width-1 downto 0);
+    coeff_b_dat_i                       : in signed(g_b_width-1 downto 0);
+    -- Input k
+    coeff_k_addr_i                      : in std_logic_vector(g_k_width-1 downto 0);
     -- Output k
-    coeff_x_addr_o                      : out unsigned(9-1 downto 0);
+    coeff_k_addr_o                      : out std_logic_vector(g_k_width-1 downto 0);
     -- Result output
     c_o                                 : out signed(g_c_width-1 downto 0);
-    -- Data valid output for debugging`
+    -- Data valid output for debugging
     valid_debug_o                       : out std_logic;
     -- Validate the end of fofb cycle
     valid_end_o                         : out std_logic
@@ -103,10 +87,10 @@ architecture behave of mac_fofb is
 
   signal clr_s, v_i_s, v_o_s            : std_logic := '0';
   signal a_s                            : signed(g_a_width-1 downto 0)   := (others => '0');
-  signal coeff_x_dat_s                  : signed(g_b_width-1 downto 0)   := (others => '0');
-  signal coeff_x_addr_s                 : unsigned(g_k_width-1 downto 0) := (others => '0');
+  signal coeff_b_dat_s                  : signed(g_b_width-1 downto 0)   := (others => '0');
+  signal coeff_k_addr_s                 : std_logic_vector(g_k_width-1 downto 0) := (others => '0');
   signal coeff_y_dat_s                  : signed(g_b_width-1 downto 0)   := (others => '0');
-  signal coeff_y_addr_s                 : unsigned(g_k_width-1 downto 0) := (others => '0');
+  signal coeff_y_addr_s                 : std_logic_vector(g_k_width-1 downto 0) := (others => '0');
   signal cnt                            : integer := 0;
 
 begin
@@ -118,7 +102,7 @@ begin
     clear_acc_i => clr_s,
     valid_i     => v_i_s,
     a_i         => a_s,
-    b_i         => coeff_x_dat_s,
+    b_i         => coeff_b_dat_s,
     c_o         => c_o,
     valid_o     => v_o_s
     );
@@ -128,19 +112,19 @@ begin
     if (rising_edge(clk_i)) then
       if rst_n_i = '0' then
         a_s           <= (others => '0');
-        coeff_x_dat_s <= (others => '0');
+        coeff_b_dat_s <= (others => '0');
         clr_s         <= '0';
         cnt           <=  0;
         valid_end_o   <= '0';
         valid_debug_o <= '0';
 
       else
-        coeff_x_addr_s  <= unsigned(fod_dat_i(g_packet_size-1 downto (2*g_a_width))); -- signal to be send to coeff_x_addr_i from wrapper
-        coeff_x_dat_s   <= coeff_x_dat_i;                                             -- signal to receive coeff_x_dat_o from wrapper
-        a_s             <= signed(fod_dat_i((2*g_a_width-1) downto g_a_width));       -- a_s will be delayed to wait for coeff_x_dat_s
+        coeff_k_addr_s  <= coeff_k_addr_i; -- signal to be send to coeff_x_addr_i from wrapper
+        coeff_b_dat_s   <= coeff_b_dat_i;  -- signal to receive coeff_x_dat_o from wrapper
+        a_s             <= coeff_a_dat_i;  -- a_s and valid bit will be delayed to wait for coeff_x_dat_s
         v_i_s           <= valid_i;
         valid_debug_o   <= v_o_s;
-        coeff_x_addr_o  <= coeff_x_addr_s;
+        coeff_k_addr_o  <= coeff_k_addr_s;
 
         if v_o_s = '1' then
           if (cnt < g_mac_size) then
--- matmul/testbench/mult_tb.vhd
+++ matmul/testbench/mult_tb.vhd
@@ -1,34 +1,19 @@
-------------------------------------------------------------------------------
-
+-------------------------------------------------------------------------------
 -- Title      :  Matrix multiplication testbench
-
-------------------------------------------------------------------------------
-
+-------------------------------------------------------------------------------
 -- Author     :  Melissa Aguiar
-
 -- Company    : CNPEM LNLS-DIG
-
 -- Platform   : FPGA-generic
-
 -------------------------------------------------------------------------------
-
 -- Description:  Testbench for the matrix multiplication core
-
 -------------------------------------------------------------------------------
-
 -- Copyright (c) 2020 CNPEM
-
 -- Licensed under GNU Lesser General Public License (LGPL) v3.0
-
 -------------------------------------------------------------------------------
-
 -- Revisions  :
-
 -- Date        Version  Author                Description
-
--- 2021-19-07  1.0      melissa.aguiar        Created
-
-------------------------------------------------------------------------------
+-- 2021-20-07  1.0      melissa.aguiar        Created
+-------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -49,9 +34,9 @@ architecture behave of mult_tb is
   constant clk_period : time        := 0.25 ms;
 
   constant g_a_width  : natural     := 32;
+  constant g_k_width  : natural     := 9;
   constant g_b_width  : natural     := 32;
   constant g_c_width  : natural     := 32;
-  constant g_packet_size : natural  := 73;
 
   signal clk_s        : std_logic   := '0';
   signal rst_s        : std_logic   := '0';
@@ -59,14 +44,13 @@ architecture behave of mult_tb is
   signal v_o_s        : std_logic   := '0';
   signal valid_tr     : std_logic   := '0';
 
-  signal k_s          : unsigned(9-1 downto 0)                     := (others => '0');
+  signal a_s          : signed(g_a_width-1 downto 0)               := (others => '0');
+  signal k_s          : std_logic_vector(g_k_width-1 downto 0)     := (others => '0');
   signal b_s          : signed(g_b_width-1 downto 0)               := (others => '0');
   signal c_s          : signed(g_c_width-1 downto 0)               := (others => '0');
 
   signal c_acc_s      : signed(g_c_width-1 downto 0)               := (others => '0');
   signal my_out_s     : signed(g_c_width-1 downto 0)               := (others => '0');
-
-  signal fod_dat_s    : std_logic_vector(g_packet_size-1 downto 0) := (others => '0');
 
 begin
 
@@ -75,9 +59,9 @@ begin
     clk_i         => clk_s,
     rst_n_i       => rst_s,
     valid_i       => v_i_s,
-    fod_dat_i     => fod_dat_s,
-    coeff_x_dat_i => b_s,
-    coeff_x_addr_o=> k_s,
+    coeff_a_dat_i => a_s,
+    coeff_b_dat_i => b_s,
+    coeff_k_addr_i=> k_s,
     c_o           => c_s,
     valid_debug_o => v_o_s
     );
@@ -103,32 +87,34 @@ begin
 
   input_read : process(clk_s)
 
-    file fod_data_file     : text open read_mode is "fofb_fod.txt";
-    variable fod_datain    : bit_vector(g_packet_size-1 downto 0);
-    variable fod_line      : line;
-
-    file b_data_file       : text open read_mode is "b_k.txt";
-    variable b_datain      : integer;
-    variable b_line        : line;
+   file a_data_file                      : text open read_mode is "a_k.txt";
+   file b_data_file                      : text open read_mode is "b_k.txt";
+   file k_data_file                      : text open read_mode is "k.txt";
+   variable a_line, b_line, k_line       : line;
+   variable a_datain, b_datain           : integer;
+   variable k_datain                     : bit_vector(g_k_width-1 downto 0);
 
     begin
       if rising_edge(clk_s) then
         rst_s <= '1';
 
-        if not endfile(fod_data_file) and valid_tr = '1' then
-          -- Reading input fod_dat_i from a txt file
-          readline(fod_data_file, fod_line);
-          read(fod_line, fod_datain);
+        if not endfile(a_data_file) and valid_tr = '1' then
+        -- Reading input a[k] from a txt file
+        readline(a_data_file, a_line);
+        read(a_line, a_datain);
 
-          -- Pass the variable to a signal
-          fod_dat_s <= to_stdlogicvector(fod_datain);
+        -- Reading input k from a txt file
+        readline(k_data_file, k_line);
+        read(k_line, k_datain);
 
-          -- Reading input b_i from a txt file
-          readline(b_data_file, b_line);
-          read(b_line, b_datain);
+        -- Reading input b[k] from a txt file
+        readline(b_data_file, b_line);
+        read(b_line, b_datain);
 
-          -- Pass the variable to a signal
-          b_s <= to_signed(b_datain, b_s'length);
+        -- Pass the variable to a signal
+        a_s <= to_signed(a_datain, a_s'length);
+        k_s <= to_stdlogicvector(k_datain);
+        b_s <= to_signed(b_datain, b_s'length);
 
           -- Update valid input bit
           v_i_s <= '1';
